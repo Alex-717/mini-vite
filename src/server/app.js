48aa55349be741c8e0b66a50395d1cb8c371d984
Alex-717 <liangyuhao717@163.com>
2024-07-29 19:23:42 +0800

    app.use链式调用

--- src/server/app.js
+++ src/server/app.js
@@ -1,12 +1,13 @@
 
 const Koa = require('koa')
 const app = new Koa()
-const PORT = 10086
+const PORT = 8001
 
-app.use(require('./mws/mw-html'))
-app.use(require('./mws/mw-js'))
-app.use(require('./mws/mw-bareImport'))
-app.use(require('./mws/mw-vue'))
+app
+  .use(require('./mws/mw-html'))
+  .use(require('./mws/mw-js'))
+  .use(require('./mws/mw-bareImport'))
+  .use(require('./mws/mw-vue'))
 
 app.listen(PORT, err => {
   console.log(`服务启动成功，端口：${PORT}`)
